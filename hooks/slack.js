32ed0b4d938e3407218ccc57b25e72ca1c18dcc4
Majid <mkhan3189@gmail.com>
2018-02-16 22:32:13 +1100

    channel name

--- hooks/slack.js
+++ hooks/slack.js
@@ -62,12 +62,12 @@ function SlackHook(config) {
             request.text = this.formatText(data.line, this.config.template || '%line');
         }
         this.slack.webhook(merge({
-            channel: data.channel,
+            channel: data.channel_name,
             username: this.config.username || 'resitail',
         }, request), (err, response) => {
             if (err || response.status === 'fail') {
                 if (typeof response !== "undefined") {
-                    console.log(response.response + ' - channel: ' + data.channel);
+                    console.log(response.response + ' - channel: ' + data.channel_name);
                 }
             }
         });
