0c4406d8e6b908bc8e55f687002fc4685a898950
Majid <mkhan3189@gmail.com>
2018-02-18 02:07:48 +1100

    Revert "require specific ver"
    This reverts commit bc72b4c7b86eb50c550a371b3add79081bb76aca.

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ for (let i = 0; i < residue_config.known_clients.length; ++i) {
     });
 }
 
-const activateHook = (hookName, path, version, config) => {
-    const hook = require(`${path}@${version}`);
+const activateHook = (hookName, path, config) => {
+    const hook = require(path);
     try {
         const hookObj = hook(config, serverInfo);
         hookObj.hookName = hookName;
@@ -83,9 +83,9 @@ config.hooks.forEach((h) => {
         const version = h.version || 'latest';
         console.log(`Downloading ${h.package}@${version}`);
         child_process.execSync(`npm install -g ${h.package}@${version}`,{stdio:[0,1,2]});
-        activateHook(h.name, h.package, version, h.config);
+        activateHook(h.name, h.package, h.config);
     } else if (!isEmpty(h.path)) {
-        activateHook(h.name, h.path, 'latest', h.config);
+        activateHook(h.name, h.path, h.config);
     } else {
         console.error(`Invalid hook ${h.name}`)
     }
